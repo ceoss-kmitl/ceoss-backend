44f7f6b94f99080167b41b622fd87c6e2d038066
bloodzmoon <thanyathon.dev@gmail.com>
2021-08-13 14:48:04 +0700

    [GH-20] Add endpint for checking web scrap result

--- src/controllers/webScrap.ts
+++ src/controllers/webScrap.ts
@@ -72,4 +72,15 @@ export class WebScrapController {
 
     return 'OK'
   }
+
+  @Get('/web-scrap/not-save')
+  async scrapDataFromRegKMITLNotSaveToDatabase() {
+    const URL =
+      'http://www.reg.kmitl.ac.th/teachtable_v20/teachtable_show.php?midterm=0&faculty_id=01&dept_id=05&curr_id=19&curr2_id=06&year=2563&semester=1'
+    const webScrap = new WebScrap(URL)
+    await webScrap.init()
+    const data = await webScrap.extractData()
+
+    return data
+  }
 }
